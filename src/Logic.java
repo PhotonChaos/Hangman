2a336eeab4afec0bb103133a0f9fd8a397c8abe8
it-is-pizza-time-my-dudes <37551335+it-is-pizza-time-my-dudes@users.noreply.github.com>
2018-03-21 14:57:28 -0400

    creating skeleton for Logic along with To do comments

--- src/Logic.java
+++ src/Logic.java
@@ -1,4 +1,14 @@
 public class Logic
 {
-    
+    public static String givePhrase()
+    {
+        //TODO: variable creation 
+        //TODO: create an array of words 
+        //TODO: pull a random one
+    }
+    public static void guessProcessing(String input)
+    {
+        //TODO: check gusses to se how they stack up to phrase
+        //TODO: updade main guess variable
+    }
 }
