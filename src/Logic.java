7f051c72538402c006e213d8de893dfef5f25114
it-is-pizza-time-my-dudes <37551335+it-is-pizza-time-my-dudes@users.noreply.github.com>
2018-03-21 15:05:38 -0400

    added new method

--- src/Logic.java
+++ src/Logic.java
@@ -6,6 +6,11 @@ public class Logic
         //TODO: create an array of words 
         //TODO: pull a random one
     }
+    public satic String firstProcessing(String Phrase)
+    {
+        //TODO: remove puncuation 
+        //TODO: return them 
+    }    
     public static void guessProcessing(String input)
     {
         //TODO: check gusses to se how they stack up to phrase
