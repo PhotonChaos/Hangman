598ea0b5459382bc7ff28b93811a44657a8a0743
PhotonChaos <calumgcoppack@gmail.com>
2018-03-21 17:59:15 -0400

    Fixed many typos

--- src/Logic.java
+++ src/Logic.java
@@ -1,19 +1,25 @@
-public class Logic
-{
-    public static String givePhrase()
-    {
-        //TODO: variable creation 
-        //TODO: create an array of words 
-        //TODO: pull a random one
+/**
+ * @author Justin Heuston
+ */
+
+public class Logic {
+    public static String givePhrase() {
+        // TODO: variable creation
+        // TODO: create an array of words
+        // TODO: pull a random one
+
+        return "";
     }
-    public static String firstProcessing(String Phrase)
-    {
-        //TODO: remove puncuation 
-        //TODO: return them 
-    }    
-    public static void guessProcessing(String input)
-    {
-        //TODO: check gusses to se how they stack up to phrase
-        //TODO: updade main guess variable
+
+    public static String firstProcessing(String phrase) {
+        // TODO: remove punctuation
+        // TODO: return them
+
+        return "";
+    }
+
+    public static void guessProcessing(String input) {
+        // TODO: check guesses to se how they stack up to phrase
+        // TODO: update main guess variable
     }
 }
