604c77c06a9b80f85b97aec42b3a4951485f81a6
PhotonChaos <calumgcoppack@gmail.com>
2018-04-04 14:35:56 -0400

    Fixed Guess Processing

--- src/Game.java
+++ src/Game.java
@@ -6,10 +6,10 @@ public class Game {
     public static final int maxGuesses = 8;
     private final String word; 
     
-    private char[] letters = new char[23];
-    private String guessed;
+    private char[] wrongGuesses;
+    private String guessedWord;
     private int guessesLeft = maxGuesses;
-    private boolean isMultiplayer = False;
+    private boolean isMultiplayer = false;
 
     // Constructors
     
@@ -18,6 +18,7 @@ public class Game {
      **/
     Game(String _word) {
         word = _word;
+        wrongGuesses = new char[guessesLeft];
     }
 
     /**
@@ -27,6 +28,12 @@ public class Game {
     Game(String _word, int guesses) {
         word = _word;
         guessesLeft = guesses;
+
+        if (guesses >= 23) {
+	        wrongGuesses = new char[23];
+        } else {
+        	wrongGuesses = new char[guessesLeft];
+    	}
     }
 
     /**
@@ -34,29 +41,45 @@ public class Game {
       * @param guesses the number of guesses the player has
       * @param multiplayer if the game is in multiplayer mode
       **/
-    Game(String word, int guesses, boolean multiplayer) {
+    Game(String _word, int guesses, boolean multiplayer) {
 		word = _word;
         guessesLeft = guesses;
         isMultiplayer = multiplayer;
+
+        if (guesses >= 23) {
+	        wrongGuesses = new char[23];
+        } else {
+        	wrongGuesses = new char[guessesLeft];
+    	}
     }
     
+
     // Methods
     
     public String processGuess(String guess) {
     	String errorMessage = "";
+    	boolean b = false;
 
         if(guess.length() != 1) {
         	errorMessage = "Please enter a valid input!";
-        } else {
-            if(word.contains(guess)) {
-                for(int i = 0; i < word.length(); i++) {
-                    if(word.toCharArray()[i] == guess.toCharArray()[0]) {
-                        guess = guess.substring(0, i) + guess + guess.substring(i+1);
-                    }
-                }
-            } else {
-                guessesLeft--;
-            }
+    	} else {
+    		for (char c : "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()) {
+    			if(Character.toUpperCase(guess.toCharArray()[0]) == c) b = true;
+    		}
+
+    		if(b) {
+	            if(word.contains(guess)) {
+	                for(int i = 0; i < word.length(); i++) {
+	                    if(word.toCharArray()[i] == guess.toCharArray()[0]) {
+	                        guess = guess.substring(0, i) + guess + guess.substring(i+1);
+	                    }
+	                }
+	            } else {
+	                guessesLeft--;
+	            }
+	        } else {
+	        	errorMessage = "Please enter a valid input!";
+	        }
         }
 
        	return errorMessage;
