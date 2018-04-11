8596970a015dbe60914f6f0d696ed3ba0a3a47bb
PhotonChaos <calumgcoppack@gmail.com>
2018-04-11 13:49:51 -0400

    Fixed EVERYTHINGGGGGGGGHGGGG&PIWEGEFOIUSDGFOHGV

--- src/Game.java
+++ src/Game.java
@@ -4,12 +4,21 @@
  **/
 public class Game {
     public static final int maxGuesses = 8;
-    private final String word;
-
-    public char[] wrongGuesses;
-    public String guessedWord;
+    public int mGuess = 8;
+    public final String word;
+    
+    public boolean isFinished = false;
+    public boolean isWon = false;
+    
+    int scoreA = 0;
+    int scoreB = 0;
+    
+    public char pTurn = '0';
+    
+    public String wrongGuesses = "";
+    public String guessedWord = "";
     public int guessesLeft = maxGuesses;
-    private boolean isMultiplayer = false;
+    public boolean isMultiplayer = false;
 
     // Constructors
 
@@ -21,14 +30,12 @@ public class Game {
 
         // parse for punctuation
         for (char c : _word.toCharArray()) {
-            if("!@#$%^&*()\'\";:[]{}\\|-=+<>,.?/~` ".contains(String.valueOf(c))) {
+            if(Character.isLetter(c)) {
                 guessedWord += '_';
             } else {
                 guessedWord += c;
             }
         }
-
-        wrongGuesses = new char[guessesLeft];
     }
 
     /**
@@ -41,17 +48,11 @@ public class Game {
 
         // parse for punctuation
         for (char c : _word.toCharArray()) {
-            if("!@#$%^&*()\'\";:[]{}\\|-=+<>,.?/~` ".contains(String.valueOf(c))) {
+            if(Character.isLetter(c)) {
                 guessedWord += '_';
             } else {
                 guessedWord += c;
             }
-        }
-
-        if (guesses >= 23) {
-            wrongGuesses = new char[23];
-        } else {
-            wrongGuesses = new char[guessesLeft];
         }
     }
 
@@ -63,58 +64,95 @@ public class Game {
     Game(String _word, int guesses, boolean multiplayer) {
         word = _word;
         guessesLeft = guesses;
+        mGuess = guessesLeft;
         isMultiplayer = multiplayer;
 
         // parse for punctuation
         for (char c : _word.toCharArray()) {
-            if("!@#$%^&*()\'\";:[]{}\\|-=+<>,.?/~` ".contains(String.valueOf(c))) {
+            if(Character.isLetter(c)) {
                 guessedWord += '_';
             } else {
                 guessedWord += c;
             }
         }
-
-        if (guesses >= 23) {
-            wrongGuesses = new char[23];
-        } else {
-            wrongGuesses = new char[guessesLeft];
-        }
-
-        System.out.println("WordG: "+guessedWord);
-        System.out.println("Word:  "+word);
+        
+        System.out.println(guessedWord);
+        //System.out.println("Word:  "+word);
     }
 
 
     // Methods
 
     public String processGuess(String guess) {
-        System.out.println("WordG: "+guessedWord);
-        System.out.println("Word:  "+word);
         String errorMessage = "";
-        boolean b = false;
-
-        if(guess.length() != 1) {
-            errorMessage = "Please enter a valid input!"; // guess is invalid
-        } else {
-            for (char c : "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()) {
-                if(Character.toUpperCase(guess.toCharArray()[0]) == c) b = true;
+        
+        if(isMultiplayer) {
+            if(pTurn == 'a') {
+                pTurn = 'b';
+            } else {
+                pTurn = 'a';
             }
+        }
 
-            if(b) {
-                if(word.contains(guess)) {
-                    for(int i = 0; i < word.length(); i++) {
-                        if(word.toCharArray()[i] == guess.toCharArray()[0]) {
-                            guessedWord = String.format("%s%s%s", guessedWord.substring(0, i), guess, guessedWord.substring(i)); //
+        if(guess.length() != 1 || !Character.isLetter(guess.charAt(0))) {
+            if(guess.length() > 1) {
+                if(guess.toLowerCase().equals(word)) {
+                    isFinished = true;
+                    if(isMultiplayer) {
+                        int scoreAdd = 0;
+                        for(char c : guessedWord.toCharArray()) {
+                            if(c == '_') scoreAdd++;
                         }
+                        
+                        if(pTurn == 'a') {
+                            scoreA += scoreAdd;
+                        } else {
+                            scoreB += scoreAdd;
+                        }
+                    } else {
+                        isWon = true;
                     }
-                } else {
-                    guessesLeft--; // Guess is valid, but incorrect
                 }
             } else {
                 errorMessage = "Please enter a valid input!"; // guess is invalid
             }
+        } else {
+            if(word.contains(guess)) {
+                if(!guessedWord.contains(guess)) {
+                    System.out.println("Correct!");
+                    for(int i = 0; i < word.length(); i++) {
+                        if(word.toCharArray()[i] == guess.toCharArray()[0]) {
+                            StringBuilder sb = new StringBuilder(guessedWord);
+                            sb.setCharAt(i, guess.toCharArray()[0]);
+                            guessedWord = sb.toString();
+                            
+                            if(isMultiplayer) {
+                                if(pTurn == 'a') {
+                                    scoreA++;
+                                } else if(pTurn == 'b') {
+                                    scoreB++;
+                                }
+                            }
+                        }
+                    }
+                    if(guessedWord.equals(word)) {
+                        isFinished = true;
+                        isWon = true;
+                    }
+                }
+            } else {
+                if(!wrongGuesses.contains(guess)) {
+                    guessesLeft--; // Guess is valid, but incorrect
+                    wrongGuesses += guess;
+                    System.out.println("\nIncorrect.");
+                }
+                if(guessesLeft <= 0) {
+                    isFinished = true; // end game
+                    isWon = false;     // player has lost the game
+                }
+            }
         }
-
+  
         return errorMessage;
     }
 }
